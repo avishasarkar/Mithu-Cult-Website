9b9b66c8765f280f9293298f13cdd5ed335ac256
Avisha Sarkar <avisha.sarkar@gmail.com>
2025-08-03 08:09:48 +0000

    Added Bootstrap and rendering dynamic data

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+POSITIONS = [
+    {
+        'id': 1,
+        'title': 'Flock Member',
+        'location': 'Durgapur',        
+    },
+    {
+        'id': 2,
+        'title': 'Avian Manager',
+        'location': 'Katpadi',        
+    },
+    {
+        'id': 3,
+        'title': 'Flock Member',
+        'location': 'Kolkata',        
+    },
+    {
+        'id': 4,
+        'title': 'Seed Specialist',
+        'location': 'Tettu',        
+    }
+    
+]
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', positions=POSITIONS,)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug = True)
